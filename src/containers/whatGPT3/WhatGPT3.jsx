37cd29a8763c16140d5a8be90e5f0aafcfc0a728
topzyray <topzyray@yahoo.com>
2023-06-06 21:59:30 +0100

    feature: adding header and brand components

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './whatGPT3.css';
+import { Feature } from '../../components';
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className='gpt3__whatgpt3 section__margin' id="wgpt3">
+      <div className='gpt3__wahtgpt3-feacture'>
+        <Feature />
+      </div>
+    </div>
   )
 }
 
